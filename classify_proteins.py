e1979b2a080fd924e49a1323ad99d4732169944c
lhugens <leonardohugens@protonmail.com>
2024-05-18 14:49:30 +0100

    dataframe correctly created

--- classify_proteins.py
+++ classify_proteins.py
@@ -1,18 +1,50 @@
+'''
+Group A
+Leonardo Hügens up201705764
+Hynek Šabacký up202311458
+Lohras Karimi up202311453
+'''
+
+'''
+Goal: given the protein sequences from two protein families
+(globin and zinc finger), create a predictive model for a 
+binary classification problem.
+'''
+
 import sklearn 
-import pandas
+import pandas as pd
 
 def file_to_seqs(filepath):
     file = open(filepath)
     lines = "".join(file.readlines())
-    sequences = []
-    seq = ""
-    for line in lines.splitlines():
-        if line.startswith(">sp"):
-            sequences.append(seq)
-            seq = ""
-        else: 
-            seq += line
-    return sequences[1:]
+    seqs = {}
+    for line in lines.split("\n"):
+        if ">sp" in line:
+            id = line.split("|")[1]
+            seqs[id] = seqs.get(id, "") 
+        else:
+            seqs[id] += line
+    return seqs
+
+def gen_all_2mers():
+    amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 
+               'E', 'G', 'H', 'I', 'L', 'K', 
+               'M', 'F', 'P', 'S', 'T', 'W', 
+               'Y', 'V']
+    all = []
+    for a in amino_acids:
+        for b in amino_acids:
+            if a+b not in all:
+                all.append(a+b)
+    return all
+
+def create_df():
+    columns = gen_all_2mers()
+    data = [[0] * len(columns)]
+    df = pd.DataFrame(data, columns=columns)
+    return df
 
 globin_seqs = file_to_seqs("./sequences/globin.fasta")
 zinc_seqs = file_to_seqs("./sequences/zincfinger.fasta")
+
+print(create_df())
